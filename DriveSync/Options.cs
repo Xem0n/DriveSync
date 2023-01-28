e12563779ebe21b3f144d30cb812864fff18c2f9
Xem0n <biedukiewicz87@gmail.com>
2023-01-28 16:35:42 +0100

    add config option

--- DriveSync/Options.cs
+++ DriveSync/Options.cs
@@ -1,6 +1,9 @@
+using CommandLine;
+
 namespace DriveSync;
 
 public class Options
 {
-    
+    [Option('c', "config", Required = false, HelpText = "Set path to config.\nDefault path: ~/.config/drivesync/config.json")]
+    public string Config { get; set; }
 }
