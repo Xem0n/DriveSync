d524a727ca13f8c9601307b24d7b0bd4ad521b9b
Xem0n <biedukiewicz87@gmail.com>
2023-01-28 23:01:01 +0100

    create template

--- DriveSync/Config.cs
+++ DriveSync/Config.cs
@@ -1,8 +1,10 @@
+using System.Text;
+
 namespace DriveSync;
 
 public class Config
 {
-    private const string DEFAULT_PATH = "~/.config/drivesync/config.json";
+    private const string DEFAULT_PATH = "/.config/drivesync/config.json";
     private const string TEMPLATE = @"
 {
     ""files\"": [
@@ -15,14 +17,46 @@ public class Config
 
     public Config(string path)
     {
-        Console.WriteLine(TEMPLATE);
-        path ??= DEFAULT_PATH;
+        path ??= $"/home/{Environment.GetEnvironmentVariable("USER")}{DEFAULT_PATH}";
 
         if (!File.Exists(path))
         {
-            // todo: create template
+            CreateTemplate(path);
+            
+            throw NewTemplateException();
         }
         
         // todo: create dict of json (or sth like that, im not sure yet)
+    }
+
+    void CreateTemplate(string path)
+    {
+        string directory = Path.GetDirectoryName(path);
+        
+        if (!Directory.Exists(directory))
+        {
+            Directory.CreateDirectory(directory);
+        }
+
+        using (FileStream fs = File.Create(path))
+        {
+            byte[] template = new UTF8Encoding(true).GetBytes(TEMPLATE);
+            
+            fs.Write(template, 0, template.Length);
+        }
+    }
+
+    InvalidConfigException NewTemplateException()
+    {
+        const string message = "Template has been created. Fill it and try again.";
+
+        return new(message);
+    }
+
+    public class InvalidConfigException : Exception
+    {
+        public InvalidConfigException() : base("Config found in your config file is invalid.") {}
+        
+        public InvalidConfigException(string message) : base(message) {}
     }
 }
