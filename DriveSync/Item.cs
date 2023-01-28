b9b44f1211fe5027dc71df3d7afe240688f7a531
Xem0n <biedukiewicz87@gmail.com>
2023-01-29 00:01:13 +0100

    add IsValid method

--- DriveSync/Item.cs
+++ DriveSync/Item.cs
@@ -5,4 +5,9 @@ public class Item
     public string Local { get; set; }
     public string Drive { get; set; }
     public string Rule { get; set; }
+
+    public bool IsValid()
+    {
+        return true;
+    }
 }
