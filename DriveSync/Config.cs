75058dd31d22554a67cdcb60ab605b4c91f2930e
Xem0n <biedukiewicz87@gmail.com>
2023-01-28 21:50:18 +0100

    add template json

--- DriveSync/Config.cs
+++ DriveSync/Config.cs
@@ -3,9 +3,19 @@ namespace DriveSync;
 public class Config
 {
     private const string DEFAULT_PATH = "~/.config/drivesync/config.json";
+    private const string TEMPLATE = @"
+{
+    ""files\"": [
+        ""local"": ""path to file on your disk"",
+        ""drive"": ""path to the file on your google drive"",
+        ""rule"": ""download/upload/sync""
+    ]
+}
+    ";
 
     public Config(string path)
     {
+        Console.WriteLine(TEMPLATE);
         path ??= DEFAULT_PATH;
 
         if (!File.Exists(path))
