20c56bae88a9b21779b3ab09819794481d26c0a2
Xem0n <biedukiewicz87@gmail.com>
2023-01-28 16:59:24 +0100

    create config

--- DriveSync/Program.cs
+++ DriveSync/Program.cs
@@ -1,1 +1,10 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using CommandLine;
+using DriveSync;
+
+CommandLine.Parser.Default.ParseArguments<Options>(args)
+    .WithParsed(Run);
+
+void Run(Options options)
+{
+    Config config = new(options.Config);
+}
