7a07de681e5dc45a1ac8476b7c6d151de958649f
Xem0n <biedukiewicz87@gmail.com>
2023-01-28 16:59:13 +0100

    set path

--- DriveSync/Config.cs
+++ DriveSync/Config.cs
@@ -3,4 +3,16 @@ namespace DriveSync;
 public class Config
 {
     private const string DEFAULT_PATH = "~/.config/drivesync/config.json";
+
+    public Config(string path)
+    {
+        path ??= DEFAULT_PATH;
+
+        if (!File.Exists(path))
+        {
+            // todo: create template
+        }
+        
+        // todo: create dict of json (or sth like that, im not sure yet)
+    }
 }
