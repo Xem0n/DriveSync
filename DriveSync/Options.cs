35b4e751cd8a2ee0ff3680d86783b3b8fb85e3c5
Xem0n <biedukiewicz87@gmail.com>
2023-01-29 18:11:50 +0100

    update help

--- DriveSync/Options.cs
+++ DriveSync/Options.cs
@@ -4,6 +4,6 @@ namespace DriveSync;
 
 public class Options
 {
-    [Option('c', "config", Required = false, HelpText = "Set path to config.\nDefault path: ~/.config/drivesync/config.json")]
+    [Option('c', "config", Required = false, HelpText = "Set path to config.\nDefault path: ~/.config/drivesync/config.json.\nAccepts only absolute paths.")]
     public string Config { get; set; }
 }
