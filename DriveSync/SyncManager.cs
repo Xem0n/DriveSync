8fa202ede0c83efacb304a09b390ea5d0e6c0bf8
Xem0n <biedukiewicz87@gmail.com>
2023-02-06 20:22:30 +0100

    add basic logic

--- DriveSync/SyncManager.cs
+++ DriveSync/SyncManager.cs
@@ -8,5 +8,30 @@ public class SyncManager
     public SyncManager(List<Item> items)
     {
         _items = items;
+        
+        AddDriveWatch();
+        AddLocalWatch();
+    }
+
+    private void AddDriveWatch()
+    {
+        foreach (var item in GetWatchItems("download"))
+        {
+            // todo: add drive watcher
+        }
+    }
+
+    private void AddLocalWatch()
+    {
+        foreach (var item in GetWatchItems("upload"))
+        { 
+            using var watcher = new FileSystemWatcher(item.Local);
+            watcher.Changed += item.OnLocalChanged;
+        }
+    }
+
+    private IEnumerable<Item> GetWatchItems(string rule)
+    {
+        return _items.Where(item => item.Rule == rule || item.Rule == "sync");
     }
 }
