6622209d4b196daa81a8464d4aedb80c434e1c75
Szymon Biedukiewicz <biedukiewicz87@gmail.com>
2024-04-08 00:07:10 +0200

    init python version

--- DriveSync/Options.cs
+++
@@ -1,9 +0,0 @@
-using CommandLine;
-
-namespace DriveSync;
-
-public class Options
-{
-    [Option('c', "config", Required = false, HelpText = "Set path to config.\nDefault path: ~/.config/drivesync/config.json.\nAccepts only absolute paths.")]
-    public string Config { get; set; }
-}
